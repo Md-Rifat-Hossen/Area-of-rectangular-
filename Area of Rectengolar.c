5e1394a86922ca76bb31e9348931bdb262e1acea
MD.RIFAT HOSSEN <121922480+Md-Rifat-Hossen@users.noreply.github.com>
2023-04-23 18:10:23 +0600

    Add files via upload

---
+++ Area of Rectengolar.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int l,h;
+    printf("Enther the lenth:\n");
+    scanf("%d",&l);
+    printf("Enther the Width:\n");
+    scanf("%d",&h);
+    printf("Area is%d\n",l*h);
+
+
+}
